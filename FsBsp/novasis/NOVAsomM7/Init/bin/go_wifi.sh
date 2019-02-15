06fff63118c2e53c16474c43d72f122f5591cc6d
NOVAsomIndustries <filippo.visocchi@novasomindustries.com>
2019-02-15 10:16:25 +0100

    fixes for wifi on m7

--- FsBsp/novasis/NOVAsomM7/Init/bin/go_wifi.sh
+++ FsBsp/novasis/NOVAsomM7/Init/bin/go_wifi.sh
@@ -2,15 +2,14 @@
 . /etc/sysconfig/wireless
 ifconfig eth0 down
 killall udhcpc
-if [ "$1" = "" ]; then
-	wpa_supplicant -i$WLAN_DEVICE -c/etc/wpa_supplicant.conf &
-else
-	wpa_supplicant -i$WLAN_DEVICE -c/etc/wpa_supplicant.conf -D$1 &
-fi
-if [ "$WLAN_USE_DHCP" == "Y" ]; then
-	udhcpc -i $WLAN_DEVICE &
-else
-	ifconfig $WLAN_DEVICE $WLAN_IP_ADDRESS up
-	route add default gw $WLAN_GATEWAY
-fi
+wpa_supplicant -Dnl80211 -i${WLAN_DEVICE} -c/etc/wpa_supplicant.conf -B
+udhcpc -i ${WLAN_DEVICE} &
+
+#Use the code below for a static ip wifi address
+#if [ "${WLAN_USE_DHCP}" == "Y" ]; then
+#	udhcpc -i ${WLAN_DEVICE} &
+#else
+#	ifconfig ${WLAN_DEVICE} ${WLAN_IP_ADDRESS} up
+#	route add default gw ${WLAN_GATEWAY}
+#fi
 
