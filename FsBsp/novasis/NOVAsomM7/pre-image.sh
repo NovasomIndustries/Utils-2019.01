39062c9ca8af6fe6625733d9c41865ba17465ee1
NOVAsomIndustries <filippo.visocchi@novasomindustries.com>
2019-02-28 12:14:44 +0100

    General fix on config files, added fs name on sysconfig, moved patches in the right place

--- FsBsp/novasis/NOVAsomM7/pre-image.sh
+++ FsBsp/novasis/NOVAsomM7/pre-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "###### Customizing file system ######"
+echo "###### Customizing $2 file system ######"
 echo "cp -r board/novasis/NOVAsomM7/Init/* ${TARGET_DIR}"
 cp -r board/novasis/NOVAsomM7/Init/* ${TARGET_DIR}
 chmod 777 ${TARGET_DIR}/bin/*
@@ -8,4 +8,5 @@ chmod 777 ${TARGET_DIR}/etc/init.d/*
 sync
 IP=`hostname -I | awk '{print $1}'`
 echo "REFERENCE_SERVER=${IP}" > board/novasis/NOVAsomM7/Init/etc/sysconfig/system_vars
+echo "FILE_SYSTEM_NAME=$2" > board/novasis/NOVAsomM7/Init/etc/sysconfig/filesystem_name
 exit $?
